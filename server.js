0a2c589bbff988328a5886e20dfee4012bf6d931
Djilou <djillou500@gmail.com>
2022-06-17 01:35:22 +0100

    nodemon and server start and routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const bodyParser = require("body-parser");
 
 const cassandra = require('cassandra-driver');
 
@@ -15,14 +16,40 @@ client.execute(query)
   .then(result => console.log('User with username %s', result.rows[1].username));
 
 /***************************************************************************************************************** */
-
 var { PythonShell } = require('python-shell');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//routes
+app.get("/",(req,res)=>{
+  //this is just a test route
+  res.json({toto:"ioi"})
+})
+app.post("/fromtotraining",(req,res)=>{
 var options = {
     scriptPath: '',
+    //replace this dates with the ones you will receive from req.body
     args: ["2022-01-16","2022-01-18"],
 };
-
 PythonShell.run('models/model.py', options, function (err, results) {
   if (err) throw err;
   console.log('results: %j', results);
 });
+//either contact your model again like this 
+//   const options2={
+//     scriptPath:'',
+//     args:["updated_db"]
+//   }
+// PythonShell.run('models/model.py', options2, function (err, results) {
+//   if (err) throw err;
+//     //and you will have the results here in the results variable
+//   console.log('results: %j', results);
+// });
+//   //return it this way
+//   res.json({...results})
+//
+  //or contact the cassandra db from here and return the results in the same way res.json({...results})
+})
+
+app.listen(8080)
